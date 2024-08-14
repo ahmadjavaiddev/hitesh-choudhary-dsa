88610fd9ad129fa9403b0ad06ba7fdcefa5d7485
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-08-14 00:39:34 -0600

    Deletion of the Node is Completed

--- BinarySearchTree/index.js
+++ BinarySearchTree/index.js
@@ -36,6 +36,54 @@ class BinarySearchTree {
         }
     }
 
+    delete(key) {
+        this.root = this.deleteNode(this.root, key);
+    }
+
+    deleteNode(node, key) {
+        if (node === null) {
+            return null;
+        }
+
+        if (key < node.key) {
+            node.left = this.deleteNode(node.left, key); // key < root => 5 line: 43 => if key > root => next line
+        } else if (key > node.key) {
+            node.right = this.deleteNode(node.right, key); // key > root => 9 line: 43 => if key > root => next line
+        } else {
+            // left and right is null then just return null
+            if (node.left === null && node.right === null) {
+                return null;
+            }
+            // If right have value but left is null then return right value of the node
+            else if (node.left === null) {
+                return node.right;
+            }
+            // If left have value but right is null then return left value of the node
+            else if (node.right === null) {
+                return node.left;
+            }
+            // If not any of the above condition is true then find min value
+            else {
+                // Hold the left node reference of the right node
+                let tempNode = this.findMinNode(node.right);
+                // Assign the left value of the right node to the node.key that is return by checking the findMinNode function which is the minimum value of the left node in the right side ({ right node })
+                node.key = tempNode.key;
+                // Now consider that root is node.right and recall deleteNode function with new root ({ node.right }) and key ({ tempNode.key })
+                node.right = this.deleteNode(node.right, tempNode.key);
+            }
+        }
+        return node;
+    }
+
+    findMinNode(node) {
+        while (node.left !== null) {
+            // Assign value of the ({ node.left }) to the node until it become null
+            node = node.left;
+        }
+        // Return node now which value is the minimum left value of the right side ({ right node })
+        return node;
+    }
+
     print() {
         console.log("Tree ::", JSON.stringify(this.root));
     }
@@ -52,4 +100,7 @@ bst.insert(2);
 bst.insert(0);
 bst.insert(9);
 bst.insert(1);
+bst.print();
+bst.delete(9);
+bst.delete(9);
 bst.print();
